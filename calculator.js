a5a5358b43690ccb1dd126c27f3db37b1d042cf0
Javiuty <javiiescuadra@gmail.com>
2020-07-20 11:20:20 +0200

    creating some logic for math operators

--- calculator.js
+++ calculator.js
@@ -13,9 +13,61 @@ function buttonClick(value) {
         //this is a number
         handleNumber(value); 
     }
+
+    screen.innerText = buffer;
 }
 
-function handleSymbol(symbol) {};
+function handleSymbol(symbol) {
+  /*   if (symbol === "C") {
+        buffer = "0";
+        runningTotal = 0;
+    } */
+
+    switch (value) {
+        case "C":
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '+':
+        case '-':
+        case '&times;':    
+        case '@divide;':
+            handleMath(symbol)
+            break;
+    }
+};
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        //Do nothing
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    bufer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer
+    } else {
+        runningTotal /= intBuffer
+    }
+}
+
 function handleNumber(numberString) {
     if (buffer === "0") {
         buffer = numberString;
@@ -24,7 +76,6 @@ function handleNumber(numberString) {
         //buffer = buffer + numberString
     }
 
-    screen.innerText = buffer;
 };
 
 function init() {
